e807d927311c2fedc2556a7772d6ce29e5a9b07d
Jihyun <wlguskim21@naver.com>
2025-04-17 14:54:56 +0900

    ✨ button 클릭 시 페이지 이동 조건 구현

--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -1,14 +1,20 @@
+import { Link } from "react-router";
 import logo from "../assets/images/logo.svg";
 import Button from "../components/Button";
 import UserCard from "../components/UserCard";
 
 function ListPage() {
+  const id = localStorage.getItem("userId");
+  const path = id ? `/post/${id}/answer` : "/";
+
   return (
     <div className="bg-grayscale-20">
       <div className="tablet:flex-row tablet:justify-between flex flex-col items-center justify-center gap-24 px-50 py-40">
         <img className="h-57 w-146" src={logo}></img>
         {/* Button 클릭 시 메인 or 답변 페이지로 이동 예정 */}
-        <Button content="답변하러 가기" path="/b" />
+        <Link to={path}>
+          <Button content="답변하러 가기" />
+        </Link>
       </div>
       <div className="tablet:flex-col tablet:gap-4 flex items-center justify-between px-24">
         <p className="tablet:text-h1 text-h3 font-regular">
