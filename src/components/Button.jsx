8fc4001c8e4663c2b43f6fe06909df52de637775
Jihyun <wlguskim21@naver.com>
2025-04-17 23:19:07 +0900

    🎨 buttonStyles 객체로 변경

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,22 +3,22 @@ function Button({ children, type = "empty", onclick }) {
   // onclick: 버튼 클릭 시 실행할 함수
   // type: 버튼 모양
 
-  function getButtonStyle() {
+  const buttonStyles = {
     // empty: 질문하러 가기, 답변하러 가기
     // fill: 질문 받기
     // round: 질문 작성하기
-    switch (type) {
-      case "empty":
-        return "bg-brown-10 border-brown-40 text-brown-40 tablet:px-24 tablet:py-12 active:bg-brown-20 flex items-center gap-4 justify-center rounded-lg border px-12 py-8 hover:border-2";
-      case "fill":
-        return "bg-brown-40 border-brown-40 text-grayscale-10 w-257 h-46 rounded-lg border tablet:w-336";
-      case "round":
-        return "bg-brown-40 border-brown-40 text-grayscale-10 tablet:w-208 px-24 py-12 rounded-full border";
-    }
-  }
+    empty:
+      "bg-brown-10 border-brown-40 text-brown-40 tablet:px-24 tablet:py-12 active:bg-brown-20 flex items-center gap-4 justify-center rounded-lg border px-12 py-8 hover:border-2",
+    fill: "bg-brown-40 border-brown-40 text-grayscale-10 w-257 h-46 rounded-lg border tablet:w-336",
+    round:
+      "bg-brown-40 border-brown-40 text-grayscale-10 tablet:w-208 px-24 py-12 rounded-full border",
+  };
 
   return (
-    <button className={getButtonStyle()} onClick={onclick}>
+    <button
+      className={buttonStyles[type] ?? buttonStyles["empty"]}
+      onClick={onclick}
+    >
       {children}
     </button>
   );
