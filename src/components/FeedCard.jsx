9e899c486f446d2f8d81e010ad2748b226a4e3c5
Moon-ju-young <amymoon07@khu.ac.kr>
2025-04-17 12:59:09 +0900

    ✨ feat: FeedCard 기본 뼈대 및 디자인 완료
    isAnswerPage는 현재 페이지가 답변 페이지 인지 확인하는 용도(변경 가능성 높음), 더보기 버튼 수정 예정

--- src/components/FeedCard.jsx
+++ src/components/FeedCard.jsx
@@ -1,3 +1,36 @@
-export default function FeedCard() {
+import Badge from "./Badge.jsx";
+import Reaction from "./Reaction.jsx";
+import iconMore from "../assets/icons/more.svg";
+
+export default function FeedCard({
+  isAnswerPage,
+  content,
+  like,
+  dislike,
+  createdAt,
+  answer,
+}) {
+  return (
+    <div className="flex flex-col gap-[1.5rem] rounded-2xl p-[1.5rem] shadow-[var(--shadow-1px)] md:gap-[2rem] md:p-[2rem]">
+      <div className="flex h-[calc(26rem/16)] justify-between">
+        <Badge />
+        {isAnswerPage && <img className="h-[calc(26rem/16)]" src={iconMore} />}
+      </div>
+      <FeedCardQuestion content={content} createdAt={createdAt} />
+      {(isAnswerPage || answer) && (
+        <FeedCardAnswer isAnswerPage={isAnswerPage} {...answer} />
+      )}
+      <div className="h-[calc(43rem/16)] border-t border-solid border-(--color-grayscale-30)">
+        <Reaction like={like} dislike={dislike} />
+      </div>
+    </div>
+  );
+}
+
+function FeedCardQuestion() {
+  return;
+}
+
+function FeedCardAnswer() {
   return;
 }
