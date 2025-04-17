13535052bd613debfc90fff15b6afddec6267108
Moon-ju-young <amymoon07@khu.ac.kr>
2025-04-17 22:06:16 +0900

    ✨ feat: FeedCardQuestion 구현 완료
    반응형 디자인까지 모두 완료

--- src/components/FeedCardQuestion.jsx
+++ src/components/FeedCardQuestion.jsx
@@ -1,3 +1,10 @@
-export default function FeedCardQuestion() {
-  return;
+import getDurationString from "../utils/getDurationString.js";
+
+export default function FeedCardQuestion({ content, createdAt }) {
+  return (
+    <div>
+      <div className="text-caption1 text-grayscale-40 mb-4 font-medium">{`질문 · ${getDurationString(createdAt)}전`}</div>
+      <div className="text-body3 tablet:text-body2">{content}</div>
+    </div>
+  );
 }
