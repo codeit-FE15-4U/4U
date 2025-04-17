9d27f5ae23df00271f90474535e28906c6c9d502
Jihyun <wlguskim21@naver.com>
2025-04-18 00:04:40 +0900

    ✨ handleButtonClick 함수 생성

--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -1,4 +1,3 @@
-// import { Link } from "react-router";
 import logo from "../assets/images/logo.png";
 import arrow from "../assets/icons/arrow.svg";
 import Button from "../components/Button";
@@ -8,18 +7,23 @@ function ListPage() {
   const id = localStorage.getItem("userId");
   const path = id ? `/post/${id}/answer` : "/";
 
+  const handleButtonClick = () => {
+    const id = localStorage.getItem("userId");
+    !id
+      ? (window.location.href = "/")
+      : (window.location.href = `/post/${id}/answer`);
+  };
+
   return (
     <div className="bg-grayscale-20">
       <div className="tablet:flex-row tablet:justify-between flex flex-col items-center justify-center gap-24 px-50 py-40">
         <img className="h-57 w-146" src={logo}></img>
         {/* Button 클릭 시 질문 받기로 생성한 id가 로컬 스토리지에 없으면 “/” 페이지로 이동 -> ok */}
         {/* Button 클릭 시 질문 받기로 생성한 id가 로컬 스토리지에 있으면 “/post/{id}/answer” 페이지로 이동 -> 확인필요 */}
-        {/* <Link to={path}> */}
-        <Button type="empty">
+        <Button type="empty" onclick={handleButtonClick}>
           답변하러 가기
-          <img src={arrow} />
+          <img className="stroke" src={arrow} />
         </Button>
-        {/* </Link> */}
       </div>
       <div className="tablet:flex-col tablet:gap-4 flex items-center justify-between px-24">
         <p className="tablet:text-h1 text-h3 font-regular">
