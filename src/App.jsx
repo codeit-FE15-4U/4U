7ab9d3281b8f15558d4a4c1d19e2413d62aefb36
YuWonKyu <duck01777@naver.com>
2025-04-17 17:12:51 +0900

    ✨ App 컴포넌트에 라우팅 추가: MainPage 및 ListPage 경로 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage";
 import ListPage from "./pages/ListPage";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/list" element={<ListPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
