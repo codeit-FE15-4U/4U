6f2fdf91b9482ab3e375b3fdef3ad6b740404192
Jihyun <wlguskim21@naver.com>
2025-04-18 15:42:33 +0900

    ✨ getUserList 함수 생성

--- src/api/subjects.js
+++ src/api/subjects.js
@@ -10,3 +10,14 @@ export const getQuestionList = async ({ subjectId, limit = 8, offset = 0 }) => {
     console.log(error.message);
   }
 };
+
+export const getUserList = async ({ limit = 6, offset = 0 }) => {
+  try {
+    const response = await axios.get(
+      `subjects/?limit=${limit}&offset=${offset}`,
+    );
+    return response;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
