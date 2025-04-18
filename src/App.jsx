cb05ed267b05cd26f9202d52969a1d71b038f675
Jihyun <wlguskim21@naver.com>
2025-04-18 19:01:50 +0900

    🔥 App.jsx에서 테스트용 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
 import "./App.css";
-<<<<<<< HEAD
-=======
 import { BrowserRouter, Routes, Route } from "react-router";
 import MainPage from "./pages/MainPage";
->>>>>>> 460fbd772deb410348a4d4c85c87af858d3edc7b
 import ListPage from "./pages/ListPage";
 
 function App() {
   return (
-<<<<<<< HEAD
-    <>
-      <ListPage />
-    </>
-=======
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/list" element={<ListPage />} />
       </Routes>
     </BrowserRouter>
->>>>>>> 460fbd772deb410348a4d4c85c87af858d3edc7b
   );
 }
 
