a6aed8cf5cec90a2d48da75e8bd9b46d13822356
YuWonKyu <duck01777@naver.com>
2025-04-19 02:45:27 +0900

    ✨ svg컴포넌트 추가할땐 테일윈드속성 값이 아니라 -> <svgIcon fill="#ffffff" />필 값 오버라이드

--- src/components/arrow-right-type2.jsx
+++ src/components/arrow-right-type2.jsx
@@ -1,27 +1,33 @@
-const ArrowRightType2 = ({ className = "" }) => {
-  return (
-    <svg
-      width="19"
-      height="18"
-      viewBox="0 0 19 18"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className={className}
-    >
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M3.6875 9C3.6875 8.68934 3.93934 8.4375 4.25 8.4375H14.75C15.0607 8.4375 15.3125 8.68934 15.3125 9C15.3125 9.31066 15.0607 9.5625 14.75 9.5625H4.25C3.93934 9.5625 3.6875 9.31066 3.6875 9Z"
-        fill="currentColor"
-      />
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M9.10225 3.35225C9.32192 3.13258 9.67808 3.13258 9.89775 3.35225L15.1477 8.60225C15.3674 8.82192 15.3674 9.17808 15.1477 9.39775L9.89775 14.6477C9.67808 14.8674 9.32192 14.8674 9.10225 14.6477C8.88258 14.4281 8.88258 14.0719 9.10225 13.8523L13.9545 9L9.10225 4.14775C8.88258 3.92808 8.88258 3.57192 9.10225 3.35225Z"
-        fill="currentColor"
-      />
-    </svg>
-  );
-};
+import React from "react";
 
-export default ArrowRightType2;
+const ArrowIcon = ({
+  className = "#ffffff fill-current",
+  width = 19,
+  height = 18,
+  fill = "#ffffff",
+}) => (
+  <svg
+    width={width}
+    height={height}
+    viewBox="0 0 19 18"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+    aria-hidden="true"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M3.6875 9C3.6875 8.68934 3.93934 8.4375 4.25 8.4375H14.75C15.0607 8.4375 15.3125 8.68934 15.3125 9C15.3125 9.31066 15.0607 9.5625 14.75 9.5625H4.25C3.93934 9.5625 3.6875 9.31066 3.6875 9Z"
+      fill={fill}
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M9.10225 3.35225C9.32192 3.13258 9.67808 3.13258 9.89775 3.35225L15.1477 8.60225C15.3674 8.82192 15.3674 9.17808 15.1477 9.39775L9.89775 14.6477C9.67808 14.8674 9.32192 14.8674 9.10225 14.6477C8.88258 14.4281 8.88258 14.0719 9.10225 13.8523L13.9545 9L9.10225 4.14775C8.88258 3.92808 8.88258 3.57192 9.10225 3.35225Z"
+      fill={fill}
+    />
+  </svg>
+);
+
+export default ArrowIcon;
