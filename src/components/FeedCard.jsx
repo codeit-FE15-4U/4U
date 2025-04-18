f854bbd955afd6b28e38a4a0b5c8d63dad278cc2
Moon-ju-young <amymoon07@khu.ac.kr>
2025-04-18 14:31:47 +0900

    ✨ feat: 오류 처리 추가
    오류 발생 시 아무것도 return 하지 않음

--- src/components/FeedCard.jsx
+++ src/components/FeedCard.jsx
@@ -5,26 +5,30 @@ import FeedCardAnswer from "./FeedCardAnswer.jsx";
 import iconMore from "../assets/icons/more.svg";
 
 export default function FeedCard({ isAnswerPage, subject, question }) {
-  return (
-    <div className="tablet:gap-32 tablet:p-32 shadow-1px font-regular text-grayscale-60 bg-grayscale-10 flex flex-col gap-24 rounded-2xl p-24">
-      <div className="flex h-26 justify-between">
-        <Badge />
-        {isAnswerPage && <img className="h-26" src={iconMore} />}
-      </div>
-      <FeedCardQuestion
-        content={question.content}
-        createdAt={question.createdAt}
-      />
-      {(isAnswerPage || question.answer) && (
-        <FeedCardAnswer
-          isAnswerPage={isAnswerPage}
-          subject={subject}
-          answer={question.answer}
+  try {
+    return (
+      <div className="tablet:gap-32 tablet:p-32 shadow-1px font-regular text-grayscale-60 bg-grayscale-10 flex flex-col gap-24 rounded-2xl p-24">
+        <div className="flex h-26 justify-between">
+          <Badge />
+          {isAnswerPage && <img className="h-26" src={iconMore} />}
+        </div>
+        <FeedCardQuestion
+          content={question.content}
+          createdAt={question.createdAt}
         />
-      )}
-      <div className="border-grayscale-30 h-43 border-t border-solid">
-        <Reaction like={question.like} dislike={question.dislike} />
+        {(isAnswerPage || question.answer) && (
+          <FeedCardAnswer
+            isAnswerPage={isAnswerPage}
+            subject={subject}
+            answer={question.answer}
+          />
+        )}
+        <div className="border-grayscale-30 h-43 border-t border-solid">
+          <Reaction like={question.like} dislike={question.dislike} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  } catch (e) {
+    return;
+  }
 }
