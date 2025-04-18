738d31e9f979ef5d3d9afc353cc73cd104a79edc
YuWonKyu <duck01777@naver.com>
2025-04-18 13:43:03 +0900

    ✏️ input 수정중, 미완성

--- src/components/MainInput.jsx
+++ src/components/MainInput.jsx
@@ -5,11 +5,22 @@ import axios from "axios";
 const MainInput = () => {
   const [name, setName] = useState("");
   const navigate = useNavigate();
+  const apiUrl = "https://openmind-api.vercel.app/15-4/subjects/";
+
+  const handleInputChange = (e) => {
+    setName(e.target.value);
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = 
+
 
   return (
     <div className="bg-grayscale-10 border-grayscale-10 flex flex-col items-center justify-center gap-16 rounded-2xl border p-16">
       <input
         type="text"
+        value={name}
         placeholder="이름을 입력하세요"
         className="border-grayscale-40 bg-grayscale-10 h-46 w-257 gap-4 rounded-lg border bg-left bg-no-repeat px-16 py-12 pl-30"
       />
