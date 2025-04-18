ec1c4bf090fe5b2688dd98ca0e23762de5d4e04f
Moon-ju-young <amymoon07@khu.ac.kr>
2025-04-18 14:23:48 +0900

    ✏️ fix: props 수정
    question prop을 구조 분해가 아니라 그냥 객체 형태로 받게 바꿈
    subject prop 추가가

--- src/components/FeedCard.jsx
+++ src/components/FeedCard.jsx
@@ -4,26 +4,26 @@ import FeedCardQuestion from "./FeedCardQuestion.jsx";
 import FeedCardAnswer from "./FeedCardAnswer.jsx";
 import iconMore from "../assets/icons/more.svg";
 
-export default function FeedCard({
-  isAnswerPage,
-  content,
-  like,
-  dislike,
-  createdAt,
-  answer,
-}) {
+export default function FeedCard({ isAnswerPage, subject, question }) {
   return (
     <div className="tablet:gap-32 tablet:p-32 shadow-1px font-regular text-grayscale-60 bg-grayscale-10 flex flex-col gap-24 rounded-2xl p-24">
       <div className="flex h-26 justify-between">
         <Badge />
         {isAnswerPage && <img className="h-26" src={iconMore} />}
       </div>
-      <FeedCardQuestion content={content} createdAt={createdAt} />
-      {(isAnswerPage || answer) && (
-        <FeedCardAnswer isAnswerPage={isAnswerPage} {...answer} />
+      <FeedCardQuestion
+        content={question.content}
+        createdAt={question.createdAt}
+      />
+      {(isAnswerPage || question.answer) && (
+        <FeedCardAnswer
+          isAnswerPage={isAnswerPage}
+          subject={subject}
+          answer={question.answer}
+        />
       )}
       <div className="border-grayscale-30 h-43 border-t border-solid">
-        <Reaction like={like} dislike={dislike} />
+        <Reaction like={question.like} dislike={question.dislike} />
       </div>
     </div>
   );
