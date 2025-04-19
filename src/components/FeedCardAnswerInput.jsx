1f53402897a95fc4d8aa6fbda53b99f4b42d09ba
Moon-ju-young <amymoon07@khu.ac.kr>
2025-04-20 04:59:09 +0900

    ✨ feat: 기본 구성

--- src/components/FeedCardAnswerInput.jsx
+++ src/components/FeedCardAnswerInput.jsx
@@ -1,5 +1,19 @@
-function FeedCardAnswerInput() {
-  return;
+import Button from "./Button";
+
+function FeedCardAnswerInput({ setState, content }) {
+  const handleClick = (e) => {
+    e.preventDefault();
+    setState("sent");
+  };
+
+  return (
+    <form>
+      {/* <InputTextArea /> */}
+      <Button type="fill" onClick={handleClick}>
+        {content ? "수정 완료" : "답변 완료"}
+      </Button>
+    </form>
+  );
 }
 
 export default FeedCardAnswerInput;
