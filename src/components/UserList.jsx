f37dc120c48ee4723729be98dd0f7202c246ab09
Jihyun <wlguskim21@naver.com>
2025-04-19 12:32:06 +0900

    🎨 user 데이터 구조분해해서 각 props로 전달하는 것으로 수정

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,11 +1,11 @@
 import messages from "../assets/icons/messages.svg";
 
-function UserCard({ user }) {
+function UserCard({ imageSource, name, questionCount }) {
   return (
     <>
       <div className="flex flex-col gap-12">
-        <img className="h-48 w-48 rounded-full" src={user.imageSource} />
-        <p className="text-body2 font-regular">{user.name}</p>
+        <img className="h-48 w-48 rounded-full" src={imageSource} />
+        <p className="text-body2 font-regular">{name}</p>
       </div>
       <div className="text-caption1 font-regular text-grayscale-40 flex items-center justify-between">
         <div className="flex items-center justify-center gap-4">
@@ -13,23 +13,29 @@ function UserCard({ user }) {
           <img className="size-16" src={messages} />
           <span>받은 질문</span>
         </div>
-        <p>{user.questionCount}개</p>
+        <p>{questionCount}개</p>
       </div>
     </>
   );
 }
 
-function UserCardList({ users }) {
+function UserList({ users }) {
   return (
     <ul className="flex flex-wrap justify-center gap-16">
       {users.map((user) => {
+        const { imageSource, name, questionCount } = user;
+
         return (
           // 카드 너비 반응형 구현 필요
           <li
             className="tablet:h-187 border-grayscale-40 bg-grayscale-10 flex h-168 w-155 flex-col justify-between rounded-xl border p-16"
             key={user.id}
           >
-            <UserCard user={user} />
+            <UserCard
+              imageSource={imageSource}
+              name={name}
+              questionCount={questionCount}
+            />
           </li>
         );
       })}
@@ -37,4 +43,4 @@ function UserCardList({ users }) {
   );
 }
 
-export default UserCardList;
+export default UserList;
