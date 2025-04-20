4ef2071037db8260681b84180f0733e3ea7ab097
YuWonKyu <duck01777@naver.com>
2025-04-21 00:53:05 +0900

    ✏️ 호버적용 및 person svgr 추가

--- src/components/MainInput.jsx
+++ src/components/MainInput.jsx
@@ -1,15 +1,16 @@
-import React from "react";
+import Person from "../assets/icons/person.svg?react";
 
 const MainInput = ({ name, setName }) => {
   return (
     <div className="relative">
+      <Person className="text-grayscale-40 absolute top-1/2 left-16 size-20 -translate-y-1/2" />
       <input
         type="text"
         placeholder="이름을 입력하세요"
         value={name}
         onChange={(e) => setName(e.target.value)}
         maxLength={15}
-        className="border-grayscale-40 bg-grayscale-10 font-weight-regular w-full rounded-lg border py-13 pl-40"
+        className="border-grayscale-40 bg-grayscale-10 focus:border-brown-40 w-full rounded-lg border-1 py-13 pr-13 pl-40 focus:outline-none"
       />
     </div>
   );
