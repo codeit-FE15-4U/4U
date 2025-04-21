08a199311b612bd031483f511d843b51f44ce53f
Jihyun <wlguskim21@naver.com>
2025-04-21 14:06:06 +0900

    🚧 브랜치 변경으로 임시 저장

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,20 +1,19 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import arrowDown from "../assets/icons/arrow-down.svg";
 import arrowUp from "../assets/icons/arrow-up.svg";
 
 function Dropdown() {
   const [isOpen, setIsOpen] = useState(false);
   const [selected, setSelected] = useState("최신순");
-  const dropdownRef = useRef(null);
   const options = [
     { label: "최신순", value: "latest" },
     { label: "이름순", value: "name" },
   ];
 
-  const toggleDown = () => {
+  const openOption = () => {
     setIsOpen(!isOpen);
-    console.log(isOpen);
   };
+
   const handleOptionClick = (option) => {
     setSelected(option.label);
     setIsOpen(false);
@@ -23,13 +22,13 @@ function Dropdown() {
   return (
     <div className="border-grayscale-40 border-1px bg-grayscale-10 rounded-lg border px-12 py-8">
       <div className="flex items-center justify-center">
-        <button className="text-grayscale-40" onClick={toggleDown}>
+        <button className="text-grayscale-40" onClick={openOption}>
           {selected}
         </button>
         <img className="h-14 w-14" src={isOpen ? arrowUp : arrowDown} />
       </div>
       {isOpen && (
-        <ul>
+        <ul className="absolute">
           {options.map((option) => {
             return (
               <li key={option.value} onClick={() => handleOptionClick(option)}>
