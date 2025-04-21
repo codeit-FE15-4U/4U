660379190de497a5db950a014c9f0c022f4abf60
leetaesik <vlwkvkd@naver.com>
2025-04-21 12:34:08 +0900

    🎨 시맨틱 태그 추가

--- src/components/QuestionLayout.jsx
+++ src/components/QuestionLayout.jsx
@@ -37,32 +37,36 @@ const QuestionLayout = () => {
   return (
     <div className="bg-grayscale-20 relative min-h-screen pb-126">
       <div className="pc:block none absolute z-0 h-234 w-full bg-white" />
-      <div className="tablet:bg-size-[1200px_234px] relative flex flex-col items-center gap-12 bg-[url(/src/assets/images/openmind-bg.png)] bg-size-[1200px_177px] bg-top bg-no-repeat px-24">
-        <Link to="/list">
-          <img
-            className="tablet:h-67 tablet:w-170 mt-40 h-49 w-124"
-            src={LogoImg}
-            alt="로고 이미지"
+      <div className="tablet:bg-size-[1200px_234px] relative flex flex-col gap-12 bg-[url(/src/assets/images/openmind-bg.png)] bg-size-[1200px_177px] bg-top bg-no-repeat px-24">
+        <header className="flex flex-col items-center gap-12">
+          <Link to="/list">
+            <img
+              className="tablet:h-67 tablet:w-170 mt-40 h-49 w-124"
+              src={LogoImg}
+              alt="로고 이미지"
+            />
+          </Link>
+          <div
+            className="tablet:size-136 size-104 rounded-full bg-cover bg-center"
+            style={{ backgroundImage: `url(${imageSource})` }}
           />
-        </Link>
-        <div
-          className="tablet:size-136 size-104 rounded-full bg-cover bg-center"
-          style={{ backgroundImage: `url(${imageSource})` }}
-        />
-        <p className="tablet:text-h2 text-h3 font-regular">{name}</p>
-        <ul className="flex gap-12">
-          <li className="bg-brown-40 size-40 rounded-full"></li>
-          <li className="size-40 rounded-full bg-yellow-50"></li>
-          <li className="size-40 rounded-full bg-blue-50"></li>
-        </ul>
+          <p className="tablet:text-h2 text-h3 font-regular">{name}</p>
+          <ul className="flex gap-12">
+            <li className="bg-brown-40 size-40 rounded-full"></li>
+            <li className="size-40 rounded-full bg-yellow-50"></li>
+            <li className="size-40 rounded-full bg-blue-50"></li>
+          </ul>
+        </header>
 
-        <Outlet
-          context={{
-            questionList,
-            questionCount,
-            subject: { name, imageSource },
-          }}
-        />
+        <main className="flex justify-center">
+          <Outlet
+            context={{
+              questionList,
+              questionCount,
+              subject: { name, imageSource },
+            }}
+          />
+        </main>
       </div>
     </div>
   );
