5f7e6182d40e7936999bb27bd6959ff29b3aaa24
leetaesik <vlwkvkd@naver.com>
2025-04-22 16:35:54 +0900

    🎨 삭제하기 버튼 디자인 수정

--- src/pages/AnswerPage.jsx
+++ src/pages/AnswerPage.jsx
@@ -1,18 +1,14 @@
 import { useOutletContext } from "react-router";
 import QuestionBox from "../components/QuestionBox";
 import FeedCard from "../components/FeedCard";
-import Button from "../components/Button";
 
 const AnswerPage = () => {
   const { questionList, questionCount, subject } = useOutletContext();
   return (
     <div className="relative w-full max-w-716">
-      <Button
-        type="round"
-        className="pc:-top-3 tablet:-top-13 absolute right-0"
-      >
+      <button className="bg-brown-40 text-grayscale-10 shadow-2pt pc:top-10 tablet:top-0 tablet:w-100 tablet:h-35 tablet:text-[15px] absolute top-23 right-0 w-70 cursor-pointer rounded-full text-[10px]/25 font-extralight">
         삭제하기
-      </Button>
+      </button>
       <QuestionBox count={questionCount}>
         <ul className="tablet:gap-20 mt-16 flex w-full flex-col gap-16">
           {questionList.map((question) => {
