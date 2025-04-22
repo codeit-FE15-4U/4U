c404a75e4a1776ef688f767739aa6b5e7d1af947
Moon-ju-young <amymoon07@khu.ac.kr>
2025-04-22 13:21:15 +0900

    ✨ feat: InputTextarea추가 및 Button disabled 속성 추가

--- src/components/FeedCardAnswerInput.jsx
+++ src/components/FeedCardAnswerInput.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import Button from "./Button";
+import InputTextarea from "./InputTextarea";
 
 function FeedCardAnswerInput({ setState, content }) {
+  const [value, setValue] = useState(content);
+
   const handleClick = (e) => {
     e.preventDefault();
     setState("sent");
@@ -8,8 +12,13 @@ function FeedCardAnswerInput({ setState, content }) {
 
   return (
     <form>
-      {/* <InputTextArea /> */}
-      <Button type="fill" onClick={handleClick}>
+      <InputTextarea
+        name="answer"
+        placeholder="답변을 입력해주세요"
+        value={value}
+        setValue={setValue}
+      />
+      <Button type="fill" onClick={handleClick} disabled={!!value}>
         {content ? "수정 완료" : "답변 완료"}
       </Button>
     </form>
