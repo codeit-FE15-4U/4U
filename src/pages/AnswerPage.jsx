0ab38b634a5cf0f4c84f12eb87a6111091ca6abc
leetaesik <vlwkvkd@naver.com>
2025-04-22 17:04:55 +0900

    ✨ 버튼 누르면 삭제하고 리스트 페이지로 이동

--- src/pages/AnswerPage.jsx
+++ src/pages/AnswerPage.jsx
@@ -1,12 +1,21 @@
-import { useOutletContext } from "react-router";
+import { useNavigate, useOutletContext } from "react-router";
 import QuestionBox from "../components/QuestionBox";
 import FeedCard from "../components/FeedCard";
+import { deleteSubject } from "../api/subjects";
 
 const AnswerPage = () => {
   const { questionList, questionCount, subject } = useOutletContext();
+  const navigate = useNavigate();
+  const handleDelete = async () => {
+    await deleteSubject({ subjectId: subject.id });
+    navigate("/list");
+  };
   return (
     <div className="relative w-full max-w-716">
-      <button className="bg-brown-40 text-grayscale-10 shadow-2pt pc:top-10 tablet:top-0 tablet:w-100 tablet:h-35 tablet:text-[15px] absolute top-23 right-0 w-70 cursor-pointer rounded-full text-[10px]/25 font-extralight">
+      <button
+        onClick={handleDelete}
+        className="bg-brown-40 text-grayscale-10 shadow-2pt pc:top-10 tablet:top-0 tablet:w-100 tablet:h-35 tablet:text-[15px] absolute top-23 right-0 w-70 cursor-pointer rounded-full text-[10px]/25 font-extralight"
+      >
         삭제하기
       </button>
       <QuestionBox count={questionCount}>
