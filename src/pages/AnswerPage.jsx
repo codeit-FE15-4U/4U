f4ca78a75d141c04a092072a8e1fd5d9991d909a
leetaesik <vlwkvkd@naver.com>
2025-04-22 21:29:01 +0900

    🎨 버튼 컴포넌트 사용

--- src/pages/AnswerPage.jsx
+++ src/pages/AnswerPage.jsx
@@ -2,6 +2,7 @@ import { useNavigate, useOutletContext } from "react-router";
 import QuestionBox from "../components/QuestionBox";
 import FeedCard from "../components/FeedCard";
 import { deleteSubject } from "../api/subjects";
+import Button from "../components/Button";
 
 const AnswerPage = () => {
   const { questionList, questionCount, subject } = useOutletContext();
@@ -17,12 +18,12 @@ const AnswerPage = () => {
   };
   return (
     <div className="relative w-full max-w-716">
-      <button
+      <Button
         onClick={handleDelete}
-        className="bg-brown-40 text-grayscale-10 shadow-2pt pc:top-10 tablet:top-0 tablet:w-100 tablet:h-35 tablet:text-[15px] absolute top-23 right-0 w-70 cursor-pointer rounded-full text-[10px]/25 font-extralight"
+        className="pc:top-10 tablet:top-0 tablet:w-100 tablet:h-35 tablet:text-[15px] absolute top-23 right-0 w-70 text-[10px]/25 font-extralight"
       >
         삭제하기
-      </button>
+      </Button>
       <QuestionBox count={questionCount}>
         <ul className="tablet:gap-20 mt-16 flex w-full flex-col gap-16">
           {questionList.map((question) => {
