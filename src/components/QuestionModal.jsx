47123fdada4b92b580e5dd7fd2e25007f55e3425
YuWonKyu <duck01777@naver.com>
2025-04-23 23:37:03 +0900

    ✨ 질문 작성 모달 UI 추가 및 상태 관리 구현

--- src/components/QuestionModal.jsx
+++ src/components/QuestionModal.jsx
@@ -1,10 +1,43 @@
+import Close from "../assets/icons/close.svg?react";
+import MessageImg from "../assets/icons/messages.svg?react";
+import Button from "./Button";
+import { useState } from "react";
+
 const QuestionModal = ({ onClose }) => {
+  const [message, setMessage] = useState("");
+
   return (
     <div className="tablet:px-78 fixed inset-0 flex items-center justify-center p-24">
       {/* 모달 배경 */}
       <div className="absolute inset-0 bg-black opacity-56" onClick={onClose} />
       {/* 모달 콘텐츠 */}
-      <div className="bg-grayscale-10 tablet:min-h-454 tablet:max-w-612 tablet:max-h-454 tablet:px-40 shadow-3pt z-10 min-h-568 w-full min-w-327 flex-col rounded-[24px] p-24"></div>
+      <div className="bg-grayscale-10 tablet:min-h-454 tablet:max-w-612 tablet:max-h-454 tablet:px-40 shadow-3pt z-10 min-h-568 w-full min-w-327 flex-col rounded-[24px] p-24">
+        <div className="flex items-center justify-between">
+          <div className="text-grayscale-60 flex gap-8 text-[20px] leading-25">
+            <MessageImg className="size-28" />
+            질문을 작성하세요
+          </div>
+          <button onClick={onClose}>
+            <Close className="tablet:size-28 size-22 cursor-pointer" />
+          </button>
+        </div>
+        <div className="mt-40">To.</div>
+        <textarea
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          className="bg-grayscale-20 tablet:min-h-180 mt-12 min-h-358 w-full resize-none border-none p-16 focus:outline-none"
+          placeholder="질문을 입력해주세요."
+        />
+
+        <Button
+          className="mt-8 w-full border-none"
+          type="fill"
+          disabled={!message}
+          onClick={onClose}
+        >
+          질문 보내기
+        </Button>
+      </div>
     </div>
   );
 };
