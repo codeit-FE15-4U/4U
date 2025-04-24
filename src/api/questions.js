2de8e32161ed1f0ec617f29a7c2defe2c4257dd5
YuWonKyu <duck01777@naver.com>
2025-04-25 02:39:35 +0900

    ✨ postQuestion 추가

--- src/api/questions.js
+++ src/api/questions.js
@@ -11,3 +11,15 @@ export const getQuestionList = async ({ subjectId, limit = 8, offset = 0 }) => {
     throw error;
   }
 };
+
+export const postQuestion = async ({ subjectId, content }) => {
+  try {
+    const response = await axios.post(`subjects/${subjectId}/questions/`, {
+      content,
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error.message);
+    throw error;
+  }
+};
