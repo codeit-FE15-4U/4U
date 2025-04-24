4422a51ac9a512569cfa92ebaa68dbfd39800f63
leetaesik <vlwkvkd@naver.com>
2025-04-24 15:17:43 +0900

    ✨ url공유 버튼 생성

--- src/components/QuestionLayout.jsx
+++ src/components/QuestionLayout.jsx
@@ -2,6 +2,7 @@ import LogoImg from "../assets/images/logo.png";
 import { Link, Outlet, useLocation, useParams } from "react-router";
 import { useEffect, useState } from "react";
 import { getQuestionList, getSubject } from "../api/subjects";
+import UrlShareButton from "./UrlShareButton";
 
 const QuestionLayout = () => {
   const [questionList, setQuestionList] = useState([]);
@@ -51,7 +52,9 @@ const QuestionLayout = () => {
           />
           <p className="tablet:text-h2 text-h3 font-regular">{name}</p>
           <ul className="flex gap-12">
-            <li className="bg-brown-40 size-40 rounded-full"></li>
+            <li>
+              <UrlShareButton />
+            </li>
             <li className="size-40 rounded-full bg-yellow-50"></li>
             <li className="size-40 rounded-full bg-blue-50"></li>
           </ul>
