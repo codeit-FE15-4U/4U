b6503bf16423e9577a87df62e011115191444d00
YuWonKyu <duck01777@naver.com>
2025-04-25 13:18:17 +0900

    🎨  UI 요소의 잘못된 태그 수정 및 스타일 일관성 개선

--- src/components/Reaction.jsx
+++ src/components/Reaction.jsx
@@ -3,16 +3,16 @@ import Thumbsdown from "../assets/icons/thumbs-down.svg?react";
 
 const Reaction = () => {
   return (
-    <ui className="flex items-center justify-center gap-32 text-[14px]">
-      <li className="text-grayscale-40 hover:text-grayscale-60 flex cursor-pointer items-center gap-6">
+    <ul className="text-caption1 flex items-center justify-center gap-32">
+      <li className="text-grayscale-40 flex cursor-pointer items-center gap-6">
         <Thumbsup className="size-16" />
         좋아요
       </li>
-      <li className="text-grayscale-40 hover:text-grayscale-60 flex cursor-pointer items-center gap-6">
+      <li className="text-grayscale-40 flex cursor-pointer items-center gap-6">
         <Thumbsdown className="size-16" />
         싫어요
       </li>
-    </ui>
+    </ul>
   );
 };
 
