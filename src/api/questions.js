5e256b546a2044680a163581b639406522a1cd02
Moon-ju-young <amymoon07@khu.ac.kr>
2025-04-25 12:53:36 +0900

    ✨ feat: deleteQuetion api 함수 추가

--- src/api/questions.js
+++ src/api/questions.js
@@ -11,3 +11,13 @@ export const getQuestionList = async ({ subjectId, limit = 8, offset = 0 }) => {
     throw error;
   }
 };
+
+export const deleteQuestion = async ({ id }) => {
+  try {
+    if (!id) throw Error("Id does not exist");
+    const response = await axios.delete(`questions/${id}/`);
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
