47502d946082effcec3dcebb5a8482c984cd0b46
Moon-ju-young <amymoon07@khu.ac.kr>
2025-04-25 20:05:07 +0900

    🐛 fix: deleteQuestion에서 try catch 문 삭제

--- src/api/questions.js
+++ src/api/questions.js
@@ -8,11 +8,7 @@ export const getQuestionList = async ({ subjectId, limit = 8, offset = 0 }) => {
 };
 
 export const deleteQuestion = async ({ id }) => {
-  try {
-    if (!id) throw Error("Id does not exist");
-    const response = await axios.delete(`questions/${id}/`);
-    return response;
-  } catch (error) {
-    console.error(error);
-  }
+  if (!id) throw Error("Id does not exist");
+  const response = await axios.delete(`questions/${id}/`);
+  return response;
 };
