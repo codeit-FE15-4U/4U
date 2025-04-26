78c20a2165d1694880b1786b8012cae4895ff8b7
Jihyun <wlguskim21@naver.com>
2025-04-26 17:40:28 +0900

    ✨ UserModal 생성

---
+++ src/components/UserModal.jsx
@@ -0,0 +1,13 @@
+function UserModal() {
+  return (
+    <div className="tablet:px-78 fixed inset-0 flex items-center justify-center p-24">
+      <div className="absolute inset-0 bg-black opacity-56">
+        <div className="bg-grayscale-10 tablet:min-h-454 tablet:max-w-612 tablet:max-h-454 tablet:px-40 shadow-3pt z-10 min-h-568 w-full min-w-327 flex-col rounded-[24px] p-24">
+          <h1>hello</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserModal;
