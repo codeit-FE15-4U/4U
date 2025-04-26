ff1ab03c003a9e7fbd1c6a15fa99044e9467127c
Moon-ju-young <amymoon07@khu.ac.kr>
2025-04-26 15:43:42 +0900

    🐛 fix: Dropdown padding 수정
    padding이 16일 경우 내부 요소들의 줄바꿈이 일어남

--- src/components/DropdownMenu.jsx
+++ src/components/DropdownMenu.jsx
@@ -8,7 +8,7 @@ function DropdownMenu({ options, selected, onSelect, isOpen, type }) {
       {options.map((option) => (
         <li
           key={option.value}
-          className={`${selected === option.label ? "text-blue-50" : "text-grayscale-50"} hover:bg-grayscale-20 px-16 py-6 first:rounded-t-lg first:pt-10 last:rounded-b-lg last:pb-10`}
+          className={`${selected === option.label ? "text-blue-50" : "text-grayscale-50"} hover:bg-grayscale-20 px-15 py-6 first:rounded-t-lg first:pt-10 last:rounded-b-lg last:pb-10`}
           onClick={() => onSelect(option)}
         >
           {option.label}
