b447d678ca4a083ca1aae5bddd1fba095dac6b9e
leetaesik <vlwkvkd@naver.com>
2025-04-28 04:53:21 +0900

    ♻️ 애니메이션 수정

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -1,17 +1,15 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 const Toast = ({ children, timer, setToast }) => {
-  const [isVisible, setIsVisible] = useState(true);
   useEffect(() => {
     setTimeout(() => {
-      setIsVisible(false);
-      setTimeout(() => setToast(false), 400);
+      setToast(false);
     }, timer);
   }, [setToast, timer]);
 
   return (
     <div
-      className={`tablet:bottom-60 bg-grayscale-60 shadow-2pt text-grayscale-10 text-caption1 fixed bottom-100 z-999 rounded-lg px-20 py-12 font-medium ${isVisible ? "animate-toastin" : "animate-toastout"}`}
+      className={`tablet:bottom-60 bg-grayscale-60 shadow-2pt text-grayscale-10 text-caption1 fixed bottom-100 z-999 animate-[0.5s_ease_toastin,0.5s_ease_4.5s_forwards_toastout] rounded-lg px-20 py-12 font-medium`}
     >
       {children}
     </div>
