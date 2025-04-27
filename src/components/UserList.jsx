707f86c3ba092e2b67597d10f5071cb54c2c15f9
Moon-ju-young <amymoon07@khu.ac.kr>
2025-04-28 00:05:14 +0900

    ✨ feat: UserList에 SkeletonUserCard 추가

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router";
 import UserCard from "./UserCard";
+import SkeletonUserCard from "./SkeletonUserCard";
 
 function UserList({ users, itemsPerPage }) {
   const navigate = useNavigate();
@@ -11,22 +12,28 @@ function UserList({ users, itemsPerPage }) {
 
   return (
     <ul className="tablet:grid-cols-[repeat(auto-fill,_minmax(186px,1fr))] pc:grid-cols-4 grid w-full max-w-928 grid-cols-2 justify-center gap-16">
-      {users.slice(0, itemsPerPage).map((user) => {
-        const { id, imageSource, name, questionCount } = user;
+      {users.length
+        ? users.slice(0, itemsPerPage).map((user) => {
+            const { id, imageSource, name, questionCount } = user;
 
-        return (
-          // 카드 너비 반응형 구현 필요
-          <li key={id}>
-            <UserCard
-              className="cursor-pointer"
-              onClick={() => handleCardClick(user)}
-              imageSource={imageSource}
-              name={name}
-              questionCount={questionCount}
-            />
-          </li>
-        );
-      })}
+            return (
+              // 카드 너비 반응형 구현 필요
+              <li key={id}>
+                <UserCard
+                  className="cursor-pointer"
+                  onClick={() => handleCardClick(user)}
+                  imageSource={imageSource}
+                  name={name}
+                  questionCount={questionCount}
+                />
+              </li>
+            );
+          })
+        : [1, 2, 3, 4, 5, 6, 7, 8].slice(0, itemsPerPage).map((num) => (
+            <li key={num}>
+              <SkeletonUserCard />
+            </li>
+          ))}
     </ul>
   );
 }
