17e97853f522c853e232aeb94907619a1d586b93
leetaesik <vlwkvkd@naver.com>
2025-04-28 04:54:41 +0900

    ♻️ 백틱 쌍따옴표로 수정

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -9,7 +9,9 @@ const Toast = ({ children, timer, setToast }) => {
 
   return (
     <div
-      className={`tablet:bottom-60 bg-grayscale-60 shadow-2pt text-grayscale-10 text-caption1 fixed bottom-100 z-999 animate-[0.5s_ease_toastin,0.5s_ease_4.5s_forwards_toastout] rounded-lg px-20 py-12 font-medium`}
+      className={
+        "tablet:bottom-60 bg-grayscale-60 shadow-2pt text-grayscale-10 text-caption1 fixed bottom-100 z-999 animate-[0.5s_ease_toastin,0.5s_ease_4.5s_forwards_toastout] rounded-lg px-20 py-12 font-medium"
+      }
     >
       {children}
     </div>
