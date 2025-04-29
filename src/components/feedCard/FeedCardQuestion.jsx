c2d7becfa192e20b1f05f798042a6f5bdaf0fedf
leetaesik <vlwkvkd@naver.com>
2025-04-29 17:35:31 +0900

    🐛 break-all속성 추가

--- src/components/feedCard/FeedCardQuestion.jsx
+++ src/components/feedCard/FeedCardQuestion.jsx
@@ -3,8 +3,8 @@ import getDurationString from "../../utils/getDurationString";
 function FeedCardQuestion({ content, createdAt }) {
   return (
     <div>
-      <div className="text-caption1 text-grayscale-40 mb-4 font-medium">{`질문 · ${getDurationString(createdAt)}전`}</div>
-      <div className="text-body3 tablet:text-body2 whitespace-pre-wrap">
+      <div className="text-caption1 text-grayscale-40 l mb-4 font-medium">{`질문 · ${getDurationString(createdAt)}전`}</div>
+      <div className="text-body3 tablet:text-body2 break-all whitespace-pre-wrap">
         {content}
       </div>
     </div>
--- src/components/user/UserCard.jsx
+++ src/components/user/UserCard.jsx
@@ -11,7 +11,9 @@ function UserCard({ imageSource, name, questionCount, className, onClick }) {
           className="tablet:size-60 size-48 rounded-full"
           src={imageSource}
         />
-        <p className="text-body2 tablet:text-body1 font-regular">{name}</p>
+        <p className="text-body2 tablet:text-body1 font-regular break-all">
+          {name}
+        </p>
       </div>
       <div className="text-caption1 font-regular text-grayscale-40 tablet:text-body3 flex items-center justify-between">
         <div className="flex items-center justify-center gap-4">
