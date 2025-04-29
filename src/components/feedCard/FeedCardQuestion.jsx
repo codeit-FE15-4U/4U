622d542fae6eda86583ce258419a490a773899bf
leetaesik <vlwkvkd@naver.com>
2025-04-29 17:42:09 +0900

    🔥 중간에 l 제거

--- src/components/feedCard/FeedCardQuestion.jsx
+++ src/components/feedCard/FeedCardQuestion.jsx
@@ -3,7 +3,7 @@ import getDurationString from "../../utils/getDurationString";
 function FeedCardQuestion({ content, createdAt }) {
   return (
     <div>
-      <div className="text-caption1 text-grayscale-40 l mb-4 font-medium">{`질문 · ${getDurationString(createdAt)}전`}</div>
+      <div className="text-caption1 text-grayscale-40 mb-4 font-medium">{`질문 · ${getDurationString(createdAt)}전`}</div>
       <div className="text-body3 tablet:text-body2 break-all whitespace-pre-wrap">
         {content}
       </div>
